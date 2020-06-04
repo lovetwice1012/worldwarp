19fe56dd2f5e913479cdc65281d63dcfdb986576
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2020-06-04 13:57:31 +0900

    Update Main.php

--- src/lovetwice1012/worldwarp/Main.php
+++ src/lovetwice1012/worldwarp/Main.php
@@ -29,7 +29,7 @@ class Main extends PluginBase implements Listener
         $customForm = new CustomForm("World Manager");
         $customForm->addLabel("Teleport to level");
         $customForm->addDropdown("Level", WMAPI::getAllLevels());
-        $sender->getHolder()->sendForm($customForm); 
+        $sender->getServer()->getPlayer()->sendForm($customForm); 
     return true;
     }  
     public static function handleCustomFormResponse(Player $player, $data, CustomForm $form) {
