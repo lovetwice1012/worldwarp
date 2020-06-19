b162ffb8a7cb6220f6de5d9c3fa71cec33764a1e
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2020-06-19 18:28:20 +0900

    Update CustomForm.php

--- src/lovetwice1012/worldwarp/CustomForm.php
+++ src/lovetwice1012/worldwarp/CustomForm.php
@@ -13,40 +13,24 @@ class CustomForm implements Form {
 
     public const FORM_CREATE = 1;
 
-    /** @var array $data */
     public $data = [];
 
-    /** @var int $mwId */
     public $mwId;
 
-    /**
-     * CustomForm constructor.
-     * @param string $title
-     */
     public function __construct(string $title = "") {
         $this->data["type"] = "custom_form";
         $this->data["title"] = $title;
         $this->data["content"] = [];
     }
 
-    /**
-     * @param string $text
-     */
     public function addInput(string $text) {
         $this->data["content"][] = ["type" => "input", "text" => $text];
     }
-
-    /**
-     * @param string $text
-     */
+    
     public function addLabel(string $text) {
         $this->data["content"][] = ["type" => "label", "text" => $text];
     }
 
-    /**
-     * @param string $text
-     * @param string|null $default
-     */
     public function addToggle(string $text, string $default = null) {
         if($default!== null) {
             $this->data["content"][] = ["type" => "toggle", "text" => $text, "default" => $default];
@@ -55,18 +39,10 @@ class CustomForm implements Form {
         $this->data["content"][] = ["type" => "toggle", "text" => $text];
     }
 
-    /**
-     * @param string $text
-     * @param array $options
-     */
     public function addDropdown(string $text, array $options) {
         $this->data["content"][] = ["type" => "dropdown", "text" => $text, "options" => $options];
     }
 
-    /**
-     * @param Player $player
-     * @param mixed $data
-     */
     public function handleResponse(Player $player, $data): void {
        Main::handleCustomFormResponse($player, $data, $this);
     }
