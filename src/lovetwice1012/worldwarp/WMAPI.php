10cbc3b7491aca675b6556aab7d9d96172b46635
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2020-06-19 18:27:22 +0900

    Update WMAPI.php

--- src/lovetwice1012/worldwarp/WMAPI.php
+++ src/lovetwice1012/worldwarp/WMAPI.php
@@ -10,8 +10,12 @@ class WMAPI {
     public static function getAllLevels(): array {
         $levels = [];
         foreach (glob(Server::getInstance()->getDataPath() . "/worlds/*") as $world) {
-            if(count(scandir($world)) >= 4) { // don't forget to .. & .
-                $levels[] = basename($world);
+            try {
+                if(count(scandir($world)) >= 4) { 
+                    $levels[] = basename($world);
+                }
+            } catch ( Exception $ex ) {
+            
             }
         }
         return $levels;
